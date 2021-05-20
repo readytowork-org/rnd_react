a9d31422a08890c9449c805bb1a53ef3876e02c9
Pradip Kharal <pradip.kharal777@gmail.com>
2021-05-20 14:39:33 +0545

    draw

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -8,23 +8,38 @@ export const Canvas = (props) => {
                         margin:auto; `
 
     const canvasRef = useRef(null)
-    var draw = ctx => {
+    var draw = (ctx, x = 0, y = 0) => {
         ctx.fillStyle = props?.textColor || '#0000ff'
         ctx.beginPath()
-        ctx.arc(props?.drawposition?.x, props?.drawposition?.y, 20, 0, 2 * Math.PI)
+        ctx.arc(x, y, 1, 0, 90)
         ctx.fill()
+    }
+    const handleMouseDown = (e) => {
+        console.error({ e, "action": "down" })
+    }
+    const drawNormal = (e, context) => {
+        console.error({ e, "action": "draw" })
+        draw(context, e.offsetX, e.offsetY)
+    }
+    const stopDrawing = (e) => {
+        console.error({ e })
     }
 
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
-        draw(context)
+        // canvas.addEventListener("mousedown", handleMouseDown)
+        // canvas.addEventListener("mousemove", drawNormal);
+        canvas.addEventListener("mousemove", (e) => { drawNormal(e, context) });
+        // canvas.addEventListener("mouseup", stopDrawing);
+        canvas.addEventListener("mouseout", stopDrawing);
+        // draw(context)
     }, [draw])
 
     return (
         <Container >
             <canvas ref={canvasRef} {...props}
-                onMouseDown={props?.onchange}
+                // onMouseDown={props?.onchange}
                 style={{ width: "100%", height: "100%", backgroundColor: "white" }}
             />
         </Container>
